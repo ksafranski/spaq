b09e59d07677a59ac592b5f71919ea7b14ef4cff
fluidbyte <kent@fluidbyte.net>
2017-02-15 10:55:29 -0500

    misc(server): remove console log

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ app.use(express.static(pubRoot))
 // Serve API
 app.all('/api/:controller/:id?', [ authentication, api, response.success ])
 
-// Supports routing on the client app
+// Support routing on the client app
 app.get('*', (req, res) => {
-  console.log('Trying to send more shit...', req.originalUrl)
   res.sendFile(path.resolve(pubRoot, 'index.html'))
 })
 
+// Handle middleware errors
 app.use(response.error)
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
