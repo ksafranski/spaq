d295b6f0d9183a6f940fb8329cb4826e881d4f96
fluidbyte <kent@fluidbyte.net>
2017-02-17 16:52:32 -0500

    feat(authentication): setting up JWT verification and decoding

--- server/lib/authentication.js
+++ server/lib/authentication.js
@@ -1,11 +1,31 @@
 const HTTPError = require('http-errors')
+const jwt = require('jwt-simple')
+
+// Removes authentication check from public API endpoints
+// Specify route as key, public methods <array> as value
+const publicRoutes = {
+  'authenticate': [ 'post' ]
+}
 
 const authentication = (req, res, next) => {
-  // Thou shall not pass
-  if (!req.headers.authorization) {
-    next(new HTTPError(403))
+  // Check if accessing public route
+  if (publicRoutes[req.params.controller] && publicRoutes[req.params.controller].indexOf(req.method.toLowerCase()) >= 0) {
+    // Attempting to access public route
+    next()
   }
-  next()
+  // Check for authorization header JWT
+  if (req.headers.authorization) {
+    // Verify JWT
+    try {
+      // Set req.token
+      req.token = jwt.decode(req.headers.authorization, process.env.AUTH_JWT_SECRET)
+      next()
+    } catch (e) {
+      next(new HTTPError(403))
+    }
+  }
+  // Default to 403
+  next(new HTTPError(403))
 }
 
 module.exports = authentication
