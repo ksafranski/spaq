bf4061e4b3679945690af6c454516520614c64f3
fluidbyte <kent@fluidbyte.net>
2017-02-22 09:56:28 -0500

    feat(httpClient): cleans up request and error handler

--- client/lib/httpClient.js
+++ client/lib/httpClient.js
@@ -1,4 +1,4 @@
-/* global sessionStorage */
+/* global sessionStorage, location */
 import axios from 'axios'
 
 // Create an Axios instance to use for all requests
@@ -11,12 +11,35 @@ const instance = axios.create({
 // Add authorization header if exists
 instance.defaults.headers.common['Authorization'] = sessionStorage.getItem('token') || null
 
-const endpoints = {
-  authenticate: (data) => instance.post(data)
-    .then((res) => {
-      // We logged in! Set the token
-      sessionStorage.setItem('token', res)
-    })
+/**
+ * Handles special case errors
+ * @param {Object.<Error>} err The caught error
+ * @returns {Object}
+ */
+const handleError = (err) => {
+  // Error messages that should kick user out of the application
+  const redirect403Errors = [
+    'Invalid Token',
+    'Token Expired',
+    'Not Authenticated'
+  ]
+  // If this is a 403 and in the redirect array, send the user to login
+  if (err.response.status === 403 && redirect403Errors.indexOf(err.response.data) >= 0) {
+    location.href = '/login'
+    return
+  }
+  // Send the error through to next catch
+  return err
 }
 
-export default endpoints
+/**
+ * Processes requests
+ * @param {String} ep The endpoint
+ * @param {String} method The HTTP method
+ * @param {*} args Spread of arguments
+ * @returns {Object.<Promise>}
+ */
+const request = (ep, method, ...args) => instance[method](ep, ...args)
+  .catch(handleError)
+
+export default request
